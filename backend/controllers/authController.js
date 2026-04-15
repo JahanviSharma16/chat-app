15e35d3f00777551593dde75130e4427c9dae371
Jahanvi <jahanvi@theebg.com>
2026-04-15 16:09:05 +0530

    update: backend

---
+++ backend/controllers/authController.js
@@ -0,0 +1,61 @@
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+// Register
+exports.register = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    // check user exists
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    // hash password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // create user
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword
+    });
+
+    res.status(201).json({ message: "User registered" });
+
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+// Login
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    // compare password
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    // generate token
+    const token = jwt.sign(
+      { id: user._id },
+      process.env.JWT_SECRET,
+      { expiresIn: "7d" }
+    );
+
+    res.json({ token });
+
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,22 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors") 
 const app = express();
+const connectDB = require("./config/db");
+
+dotenv.config();
+
+app.use(cors());
+app.use(express.json())
 
 app.get("/", (req, res) => {
-  res.send("Backend running");
+  res.send("Server is running");
 });
 
-app.listen(5000, () => console.log("Server running"));
+const PORT = process.env.PORT || 5000
+
+connectDB();
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+})
